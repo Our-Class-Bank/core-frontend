8f861fcde4380d1ae0c4272dcfaf89352c56d1bc
s0ojin <onepst@hanyang.ac.kr>
2023-06-24 01:40:49 +0900

    Feat : 헤더및 사이드바 visible fnction 추가

--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -1,15 +1,28 @@
 import LogoutBtn from "./LogoutBtn";
 import { ReactComponent as Logo } from "@/assets/images/Logo.svg";
 import { Wrapper, LogoWrapper, LogoText } from "@/style/layout/HeaderStyle";
+import { useLocation } from "react-router-dom";
+
 function Header() {
+  const location = useLocation();
+
+  const visibleHeader = () => {
+    if (location.pathname === "/login") return false;
+    return true;
+  };
+
   return (
-    <Wrapper>
-      <LogoWrapper>
-        <Logo fill="#2f3fd4" width="50" height="50" />
-        <LogoText>우리반 은행</LogoText>
-      </LogoWrapper>
-      <LogoutBtn />
-    </Wrapper>
+    <>
+      {visibleHeader() && (
+        <Wrapper>
+          <LogoWrapper>
+            <Logo fill="#2f3fd4" width="50" height="50" />
+            <LogoText>우리반 은행</LogoText>
+          </LogoWrapper>
+          <LogoutBtn />
+        </Wrapper>
+      )}
+    </>
   );
 }
 export default Header;
