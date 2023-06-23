287258d97127ed1512594ec5f3a66d4837d3a420
s0ojin <onepst@hanyang.ac.kr>
2023-06-24 02:48:15 +0900

    Feat : 로그인 UI

--- src/style/common/CommonStyle.ts
+++ src/style/common/CommonStyle.ts
@@ -4,4 +4,8 @@ export const Container = styled.div`
   width: 100dvw;
   height: 100dvh;
   background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
