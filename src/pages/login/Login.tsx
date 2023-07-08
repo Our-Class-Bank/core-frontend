b03b6f3bd5e6b6445bd301dde24ad6225d9ef0fe
s0ojin <onepst@hanyang.ac.kr>
2023-07-08 20:53:04 +0900

    feat: login api 연결

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Container } from "@/style/common/CommonStyle";
 import { Input, LoginForm, SubmitBtn } from "@/style/login/LoginStyle";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { postSignIn } from "@/apis/authApi";
 
-interface IFormValues {
+export interface IFormValues {
   id: string;
   password: string;
 }
 
 function Login() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { isValid },
   } = useForm<IFormValues>();
 
-  const submitHandler: SubmitHandler<IFormValues> = (data) => {
-    console.log(data);
+  const submitHandler: SubmitHandler<IFormValues> = async (data) => {
+    try {
+      setIsLoading(true);
+      const response = await postSignIn(data);
+      if (response.status === 200) {
+        localStorage.setItem("accessToken", response.data.token);
+        navigate("/");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 422) {
+          alert("");
+        }
+        if (error.response?.status === 401) {
+          alert("");
+        } else {
+          alert("");
+        }
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   return (
     <Container>
       <LoginForm onSubmit={handleSubmit(submitHandler)}>
