e19e27d4cad52b95f97af56497d9f1f85667172e
s0ojin <onepst@hanyang.ac.kr>
2023-08-27 16:35:05 +0900

    FEAT: private route 추가

--- src/pages/layout/LogoutBtn.tsx
+++ src/pages/layout/LogoutBtn.tsx
@@ -1,8 +1,15 @@
 import { Button, Text } from "@/style/layout/LogoutBtnStyle";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn() {
+  const navigation = useNavigate();
   return (
-    <Button>
+    <Button
+      onClick={() => {
+        localStorage.removeItem("accessToken");
+        navigation("/login");
+      }}
+    >
       <Text>로그아웃</Text>
     </Button>
   );
--- src/routes/RoutePage.tsx
+++ src/routes/RoutePage.tsx
@@ -3,13 +3,19 @@ import Login from "@/pages/login/Login";
 import Transfer from "@/pages/transfer/Transfer";
 import Home from "@/pages/home/Home";
 import Credit from "@/pages/credit/Credit";
+import PrivateRoutePage from "./privateRoutePage";
+
 function RoutePage() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/transfer" element={<Transfer />} />
-      <Route path="/credit" element={<Credit />} />
+      <Route element={<PrivateRoutePage authentication={true} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/credit" element={<Credit />} />
+      </Route>
+      <Route element={<PrivateRoutePage authentication={false} />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
